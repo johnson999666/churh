ca804d761e80f86567382190abba5f5db3b3e2ef
johnson999666 <123405062+johnson999666@users.noreply.github.com>
2023-05-08 14:40:39 -0400

    Update server.js

--- server.js
+++ server.js
@@ -18,10 +18,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-
-
-
-
-app.listen(process.env.PORT ||8080, "0.0.0.0", () => {
-  console.log('Server listening on port 3000');
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
