042354b99a487ddd980764d5328a11e63d3907c2
johnson999666 <123405062+johnson999666@users.noreply.github.com>
2023-05-08 14:12:56 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,56 +1,25 @@
+// Import dependencies
 const express = require('express');
+const path = require('path');
 
-const fetch = require('node-fetch');
-
-
-
+// Create Express app
 const app = express();
-app.use(express.static('public'));
 
-
-
+// Set the view engine to EJS
 app.set('view engine', 'ejs');
 
+// Set the path to the views folder
+app.set('views', path.join(__dirname, 'views'));
+
+// Define a route for the home page
 app.get('/', (req, res) => {
-  const city = req.query.city || 'New York';
-  const apiKey = 'c740b141159dc93d62c32a163b009d37';
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
-    .then(response => response.json())
-    .then(data => {
-      const temperature = data.main.temp;
-      const description = data.weather[0].description;
-      const weather = new Weather({
-        city,
-        temperature,
-      });
-      weather.save();
-      res.render('index', { city, temperature, description });
-    })
-    .catch(error => {
-      console.log(error);
-      res.render('error');
-    });
+  // Render the index.ejs file
+  res.render('index');
 });
 
-app.post('/weather', async (req, res) => {
-  const city = req.body.city;
-  const apiKey = 'c740b141159dc93d62c32a163b009d37';
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
-  const data = await response.json();
-  if (data.cod === '404') {
-    console.log(data.message); // log the error message
-    res.render('error', { message: 'City not found. Please try again.' });
-  } else {
-    const temperature = data.main.temp;
-    const description = data.weather[0].description;
-    const weather = new Weather({
-      city,
-      temperature,
-    });
-    weather.save();
-    res.render('weather', { city, temperature, description });
-  }
-});
+// Start the server
+
+
 
 
 app.listen(process.env.PORT ||8080, "0.0.0.0", () => {
